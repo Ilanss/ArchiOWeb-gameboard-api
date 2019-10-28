49037d86d457afeae1ad6149f135305309b0a58a
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2019-10-28 15:33:02 +0100

    usersRoutes_added

--- gameboardAPI/routes/users.js
+++ gameboardAPI/routes/users.js
@@ -3,7 +3,60 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
+});
+router.get('/users/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.get('/games', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.get('/games/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.get('/users/:id/collections', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.get('/users/:id/collections/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.get('/users/:id/collections/:id/games', function(req, res, next) {
+    res.send('respond with a resource');
 });
 
+/* POST users listing. */
+router.post('/users', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.post('/games', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.post('/users/:id/collections', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.post('/users/:id/collections/:id/games', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+/* PATCH users listing. */
+router.patch('/users/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.patch('/games/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.patch('/users/:id/collections/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+/* DELATE users listing. */
+router.delete('/users/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.delete('/games/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
+router.delete('/users/:id/collections/:id', function(req, res, next) {
+    res.send('respond with a resource');
+});
 module.exports = router;
