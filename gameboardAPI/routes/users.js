1d505440da9f84d74ec4110e231d633c02805e8a
Skyggen <adrien.ciampone@heig-vd.ch>
2019-10-31 10:10:23 +0100

    add apiDoc

--- gameboardAPI/routes/users.js
+++ gameboardAPI/routes/users.js
@@ -2,9 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
+
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
 });
+/**
+ * @api {get} /users/:id Request a user's information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Unique identifier of the user
+ *
+ * @apiSuccess {String} firstName First name of the user
+ * @apiSuccess {String} lastName  Last name of the user
+ */
 router.get('/users/:id', function(req, res, next) {
     res.send('respond with a resource');
 });
