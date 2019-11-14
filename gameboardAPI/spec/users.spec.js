0b4144bb9f8e45b8af97b16611ccaf5e54686187
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2019-11-14 12:13:38 +0100

    usersTest_getTest_implemented

--- gameboardAPI/spec/users.spec.js
+++ gameboardAPI/spec/users.spec.js
@@ -3,6 +3,7 @@ const supertest = require('supertest');
 const app = require('../app');
 const mongoose = require('mongoose');
 const { cleanUpDatabase } = require('./utils');
+const User = require('../db/models/User');
 
 //clean the database before testing
 beforeEach(cleanUpDatabase);
@@ -21,6 +22,28 @@ describe('POST /users', function() {
         expect(res.body._id).to.be.a('string');
         expect(res.body.username).to.equal('John Doe');
         expect(res.body).to.have.all.keys('_id', 'username', 'collections', 'createdAt', 'updatedAt', '__v');
+    });
+});
+
+describe('GET /users', function() {
+    beforeEach(async function() {
+        // Create 2 users before retrieving the list.
+        await Promise.all([User.create({ username: 'John Doe' }), User.create({ username: 'Jane Doe' })]);
+    });
+    it('should retrieve the list of users', async function() {
+        const res = await supertest(app).get('/users').expect(200).expect('Content-Type', /json/);
+        expect(res.body).to.be.an('array');
+        expect(res.body).to.have.lengthOf(2);
+        //test user 1
+        expect(res.body[0]).to.be.an('object');
+        expect(res.body[0]._id).to.be.a('string');
+        expect(res.body[0].username).to.equal('John Doe');
+        expect(res.body[0]).to.have.all.keys('_id', 'username', 'collections', 'createdAt', 'updatedAt', '__v');
+        //test user 2
+        expect(res.body[1]).to.be.an('object');
+        expect(res.body[1]._id).to.be.a('string');
+        expect(res.body[1].username).to.equal('Jane Doe');
+        expect(res.body[1]).to.have.all.keys('_id', 'username', 'collections', 'createdAt', 'updatedAt', '__v');
     });
 });
 
