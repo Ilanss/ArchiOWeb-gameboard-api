c61a77d0433b9e5be938c67f2ff67cb162a2aed8
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2019-11-17 16:39:11 +0100

    minimalTest_2/4

--- gameboardAPI/spec/game.spec.js
+++ gameboardAPI/spec/game.spec.js
@@ -2,9 +2,11 @@ const { expect } = require('chai');
 const supertest = require('supertest');
 const app = require('../app');
 const mongoose = require('mongoose');
-const { cleanUpDatabase } = require('./utils');
+
 const File = require('../db/models/Game');
 const Game = File.Game;
+
+const { cleanUpDatabase } = require('./utils');
 
 //clean the database before testing
 beforeEach(cleanUpDatabase);
@@ -14,21 +16,26 @@ describe('POST /games', function() {
         const res = await supertest(app)
             .post('/games')
             .send({
-                name: 'uno'
+                name: 'uno',
+                createdBy: 'testid'
             })
             .expect(201)
             .expect('Content-Type', /json/);
         expect(res.body).to.be.an('object');
         expect(res.body._id).to.be.a('string');
         expect(res.body.name).to.equal('uno');
-        expect(res.body).to.have.all.keys('_id', 'name', 'createdAt', 'updatedAt', '__v');
+        expect(res.body.createdBy).to.equal('testid');
+        expect(res.body).to.have.all.keys('_id', 'name', 'createdBy', 'createdAt', 'updatedAt', '__v');
     });
 });
 
 describe('GET /games', function() {
     beforeEach(async function() {
         // Create 2 games before retrieving the list.
-        await Promise.all([Game.create({ name: 'uno' }), Game.create({ name: 'duo' })]);
+        await Promise.all([
+            Game.create({ name: 'uno', createdBy: 'testid' }),
+            Game.create({ name: 'duo', createdBy: 'testid' })
+        ]);
     });
     it('should retrieve the list of games', async function() {
         const res = await supertest(app).get('/games').expect(200).expect('Content-Type', /json/);
--- gameboardAPI/spec/utils.js
+++ gameboardAPI/spec/utils.js
@@ -1,7 +1,6 @@
 const File = require('../db/models/User');
 const User = File.User;
-const File2 = require('../db/models/Game');
-const Game = File2.Game;
+const Game = require('../db/models/Game');
 
 exports.cleanUpDatabase = async function() {
     await Promise.all([User.deleteMany(), Game.deleteMany()]);
