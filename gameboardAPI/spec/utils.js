9c84cac4444d9898799210e82cb0f9171efeeacc
Teo Colomberotto <teo.colomberotto@heig-vd.ch>
2019-11-17 15:52:32 +0100

    Test_users_bug

--- gameboardAPI/spec/utils.js
+++ gameboardAPI/spec/utils.js
@@ -3,7 +3,6 @@ const User = File.User;
 const File2 = require('../db/models/Game');
 const Game = File2.Game;
 
-exports.cleanUpUsersDatabase = async function() {
-    await Promise.all([User.deleteMany()]);
-    await Promise.all([Game.deleteMany()]);
+exports.cleanUpDatabase = async function() {
+    await Promise.all([User.deleteMany(), Game.deleteMany()]);
 };
